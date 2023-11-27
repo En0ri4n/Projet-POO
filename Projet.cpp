687700df146176f0223f6f108ab90d6da5fa8801
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2023-11-27 16:44:02 +0100

    Add project files.

---
+++ Projet.cpp
@@ -0,0 +1,20 @@
+#include "Projet.h"
+
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+[STAThread]
+
+void main(array<String^>^ args)
+{
+
+    Application::EnableVisualStyles();
+
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    ProjetPOO::Projet form;
+
+    Application::Run(% form);
+
+}
---
+++ Projet.h
@@ -0,0 +1,96 @@
+#pragma once
+
+namespace ProjetPOO {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Projet
+	/// </summary>
+	public ref class Projet : public System::Windows::Forms::Form
+	{
+	public:
+		Projet(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Projet()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(73, 87);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(164, 71);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Clique moi";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Projet::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(400, 87);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(180, 71);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Ne clique pas";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Projet::button2_Click);
+			// 
+			// Projet
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(635, 465);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"Projet";
+			this->Text = L"Projet";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
