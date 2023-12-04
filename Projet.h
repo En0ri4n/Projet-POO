b08d1aebfc5745c420956ae73dbfc6a1999b8d82
Anthony <124313466+aschnepp@users.noreply.github.com>
2023-12-04 14:55:45 +0100

    Correction

--- Projet.h
+++ Projet.h
@@ -125,7 +125,7 @@ namespace ProjetPOO {
 			// button1
 			// 
 			this->button1->Location = System::Drawing::Point(313, 430);
-			this->button1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button1->Margin = System::Windows::Forms::Padding(4);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(240, 87);
 			this->button1->TabIndex = 0;
@@ -136,7 +136,7 @@ namespace ProjetPOO {
 			// button2
 			// 
 			this->button2->Location = System::Drawing::Point(44, 430);
-			this->button2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button2->Margin = System::Windows::Forms::Padding(4);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(240, 87);
 			this->button2->TabIndex = 1;
@@ -161,7 +161,7 @@ namespace ProjetPOO {
 			this->dataGridView->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(2) { this->Name, this->id });
 			this->dataGridView->GridColor = System::Drawing::SystemColors::ActiveBorder;
 			this->dataGridView->Location = System::Drawing::Point(953, 15);
-			this->dataGridView->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->dataGridView->Margin = System::Windows::Forms::Padding(4);
 			this->dataGridView->Name = L"dataGridView";
 			this->dataGridView->ReadOnly = true;
 			this->dataGridView->RowHeadersWidth = 51;
@@ -194,7 +194,7 @@ namespace ProjetPOO {
 			this->tabController->Controls->Add(this->tabPage5);
 			this->tabController->Controls->Add(this->tabPage6);
 			this->tabController->Location = System::Drawing::Point(16, 15);
-			this->tabController->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabController->Margin = System::Windows::Forms::Padding(4);
 			this->tabController->Name = L"tabController";
 			this->tabController->SelectedIndex = 0;
 			this->tabController->Size = System::Drawing::Size(915, 674);
@@ -217,9 +217,9 @@ namespace ProjetPOO {
 			this->tabPage1->Controls->Add(this->label3);
 			this->tabPage1->Controls->Add(this->label2);
 			this->tabPage1->Location = System::Drawing::Point(4, 25);
-			this->tabPage1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage1->Margin = System::Windows::Forms::Padding(4);
 			this->tabPage1->Name = L"tabPage1";
-			this->tabPage1->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage1->Padding = System::Windows::Forms::Padding(4);
 			this->tabPage1->Size = System::Drawing::Size(907, 645);
 			this->tabPage1->TabIndex = 0;
 			this->tabPage1->Text = L"Accueil";
@@ -240,7 +240,7 @@ namespace ProjetPOO {
 			this->button8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button8->Location = System::Drawing::Point(399, 370);
-			this->button8->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button8->Margin = System::Windows::Forms::Padding(4);
 			this->button8->Name = L"button8";
 			this->button8->Size = System::Drawing::Size(133, 37);
 			this->button8->TabIndex = 12;
@@ -265,7 +265,7 @@ namespace ProjetPOO {
 			this->passwordBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->passwordBox->Location = System::Drawing::Point(324, 326);
-			this->passwordBox->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->passwordBox->Margin = System::Windows::Forms::Padding(4);
 			this->passwordBox->Name = L"passwordBox";
 			this->passwordBox->PasswordChar = '*';
 			this->passwordBox->Size = System::Drawing::Size(369, 26);
@@ -289,7 +289,7 @@ namespace ProjetPOO {
 			this->usernameBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->usernameBox->Location = System::Drawing::Point(324, 276);
-			this->usernameBox->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->usernameBox->Margin = System::Windows::Forms::Padding(4);
 			this->usernameBox->Name = L"usernameBox";
 			this->usernameBox->Size = System::Drawing::Size(369, 26);
 			this->usernameBox->TabIndex = 8;
@@ -312,7 +312,7 @@ namespace ProjetPOO {
 			this->button7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button7->Location = System::Drawing::Point(460, 561);
-			this->button7->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button7->Margin = System::Windows::Forms::Padding(4);
 			this->button7->Name = L"button7";
 			this->button7->Size = System::Drawing::Size(260, 60);
 			this->button7->TabIndex = 6;
@@ -325,7 +325,7 @@ namespace ProjetPOO {
 			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button6->Location = System::Drawing::Point(192, 561);
-			this->button6->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button6->Margin = System::Windows::Forms::Padding(4);
 			this->button6->Name = L"button6";
 			this->button6->Size = System::Drawing::Size(260, 60);
 			this->button6->TabIndex = 5;
@@ -338,7 +338,7 @@ namespace ProjetPOO {
 			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button5->Location = System::Drawing::Point(592, 494);
-			this->button5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button5->Margin = System::Windows::Forms::Padding(4);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(284, 60);
 			this->button5->TabIndex = 4;
@@ -351,7 +351,7 @@ namespace ProjetPOO {
 			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button4->Location = System::Drawing::Point(324, 494);
-			this->button4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button4->Margin = System::Windows::Forms::Padding(4);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(260, 60);
 			this->button4->TabIndex = 3;
@@ -364,7 +364,7 @@ namespace ProjetPOO {
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button3->Location = System::Drawing::Point(29, 494);
-			this->button3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button3->Margin = System::Windows::Forms::Padding(4);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(287, 60);
 			this->button3->TabIndex = 2;
@@ -402,9 +402,9 @@ namespace ProjetPOO {
 			this->tabPage2->Controls->Add(this->button2);
 			this->tabPage2->Controls->Add(this->label1);
 			this->tabPage2->Location = System::Drawing::Point(4, 25);
-			this->tabPage2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage2->Margin = System::Windows::Forms::Padding(4);
 			this->tabPage2->Name = L"tabPage2";
-			this->tabPage2->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage2->Padding = System::Windows::Forms::Padding(4);
 			this->tabPage2->Size = System::Drawing::Size(907, 645);
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"Personnel";
@@ -414,7 +414,7 @@ namespace ProjetPOO {
 			// tabPage3
 			// 
 			this->tabPage3->Location = System::Drawing::Point(4, 25);
-			this->tabPage3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage3->Margin = System::Windows::Forms::Padding(4);
 			this->tabPage3->Name = L"tabPage3";
 			this->tabPage3->Size = System::Drawing::Size(907, 645);
 			this->tabPage3->TabIndex = 2;
@@ -424,7 +424,7 @@ namespace ProjetPOO {
 			// tabPage4
 			// 
 			this->tabPage4->Location = System::Drawing::Point(4, 25);
-			this->tabPage4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage4->Margin = System::Windows::Forms::Padding(4);
 			this->tabPage4->Name = L"tabPage4";
 			this->tabPage4->Size = System::Drawing::Size(907, 645);
 			this->tabPage4->TabIndex = 3;
@@ -434,7 +434,7 @@ namespace ProjetPOO {
 			// tabPage5
 			// 
 			this->tabPage5->Location = System::Drawing::Point(4, 25);
-			this->tabPage5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage5->Margin = System::Windows::Forms::Padding(4);
 			this->tabPage5->Name = L"tabPage5";
 			this->tabPage5->Size = System::Drawing::Size(907, 645);
 			this->tabPage5->TabIndex = 4;
@@ -444,7 +444,7 @@ namespace ProjetPOO {
 			// tabPage6
 			// 
 			this->tabPage6->Location = System::Drawing::Point(4, 25);
-			this->tabPage6->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage6->Margin = System::Windows::Forms::Padding(4);
 			this->tabPage6->Name = L"tabPage6";
 			this->tabPage6->Size = System::Drawing::Size(907, 645);
 			this->tabPage6->TabIndex = 5;
@@ -455,13 +455,13 @@ namespace ProjetPOO {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1924, 704);
+			this->ClientSize = System::Drawing::Size(1886, 704);
 			this->Controls->Add(this->tabController);
 			this->Controls->Add(this->dataGridView);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
-			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->MaximizeBox = false;
-			// this->Name = L"Projet";
+			//this->Name = L"Projet";
 			this->Text = L"Business Manager - Powered by EVAL";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView))->EndInit();
 			this->tabController->ResumeLayout(false);
@@ -470,8 +470,8 @@ namespace ProjetPOO {
 			this->tabPage2->ResumeLayout(false);
 			this->tabPage2->PerformLayout();
 			this->ResumeLayout(false);
-
 		}
+
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		this->label1->Text = "BIEN JOUE";
