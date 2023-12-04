1f4a782104317c2c4b5e34636d4fa69a9abc8097
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2023-12-04 14:37:09 +0100

    Add tabs and decorate welcome tab

--- Projet.h
+++ Projet.h
@@ -62,6 +62,16 @@ namespace ProjetPOO {
 	private: System::Windows::Forms::TabPage^ tabPage4;
 	private: System::Windows::Forms::TabPage^ tabPage5;
 	private: System::Windows::Forms::TabPage^ tabPage6;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ passwordBox;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ usernameBox;
+
+	private:
+		bool connected;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Label^ messageConnexionBDD;
 
 	protected:
 
@@ -78,6 +88,7 @@ namespace ProjetPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Projet::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
@@ -86,6 +97,13 @@ namespace ProjetPOO {
 			this->id = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->tabController = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->messageConnexionBDD = (gcnew System::Windows::Forms::Label());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->passwordBox = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->usernameBox = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->button7 = (gcnew System::Windows::Forms::Button());
 			this->button6 = (gcnew System::Windows::Forms::Button());
 			this->button5 = (gcnew System::Windows::Forms::Button());
@@ -106,7 +124,7 @@ namespace ProjetPOO {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(50, 10);
+			this->button1->Location = System::Drawing::Point(235, 349);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(180, 71);
 			this->button1->TabIndex = 0;
@@ -116,7 +134,7 @@ namespace ProjetPOO {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(50, 99);
+			this->button2->Location = System::Drawing::Point(33, 349);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(180, 71);
 			this->button2->TabIndex = 1;
@@ -137,22 +155,27 @@ namespace ProjetPOO {
 			// dataGridView
 			// 
 			this->dataGridView->AllowUserToOrderColumns = true;
+			this->dataGridView->BackgroundColor = System::Drawing::SystemColors::ActiveBorder;
 			this->dataGridView->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dataGridView->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(2) { this->Name, this->id });
+			this->dataGridView->GridColor = System::Drawing::SystemColors::ActiveBorder;
 			this->dataGridView->Location = System::Drawing::Point(715, 12);
 			this->dataGridView->Name = L"dataGridView";
+			this->dataGridView->ReadOnly = true;
 			this->dataGridView->Size = System::Drawing::Size(795, 548);
 			this->dataGridView->TabIndex = 3;
 			// 
 			// Name
 			// 
-			this->Name->HeaderText = L"Name";
+			this->Name->HeaderText = L"Projet";
 			this->Name->Name = L"Name";
+			this->Name->ReadOnly = true;
 			// 
 			// id
 			// 
 			this->id->HeaderText = L"Id";
 			this->id->Name = L"id";
+			this->id->ReadOnly = true;
 			// 
 			// tabController
 			// 
@@ -170,6 +193,13 @@ namespace ProjetPOO {
 			// 
 			// tabPage1
 			// 
+			this->tabPage1->Controls->Add(this->messageConnexionBDD);
+			this->tabPage1->Controls->Add(this->button8);
+			this->tabPage1->Controls->Add(this->label6);
+			this->tabPage1->Controls->Add(this->passwordBox);
+			this->tabPage1->Controls->Add(this->label5);
+			this->tabPage1->Controls->Add(this->usernameBox);
+			this->tabPage1->Controls->Add(this->label4);
 			this->tabPage1->Controls->Add(this->button7);
 			this->tabPage1->Controls->Add(this->button6);
 			this->tabPage1->Controls->Add(this->button5);
@@ -185,11 +215,87 @@ namespace ProjetPOO {
 			this->tabPage1->Text = L"Accueil";
 			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
+			// messageConnexionBDD
+			// 
+			this->messageConnexionBDD->AutoSize = true;
+			this->messageConnexionBDD->Location = System::Drawing::Point(332, 189);
+			this->messageConnexionBDD->Name = L"messageConnexionBDD";
+			this->messageConnexionBDD->Size = System::Drawing::Size(28, 13);
+			this->messageConnexionBDD->TabIndex = 13;
+			this->messageConnexionBDD->Text = L"";
+			this->messageConnexionBDD->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// button8
+			// 
+			this->button8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button8->Location = System::Drawing::Point(299, 301);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(100, 30);
+			this->button8->TabIndex = 12;
+			this->button8->Text = L"Connexion";
+			this->button8->UseVisualStyleBackColor = true;
+			this->button8->Click += gcnew System::EventHandler(this, &Projet::clickOnConnexionBDD);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(141, 268);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(89, 16);
+			this->label6->TabIndex = 11;
+			this->label6->Text = L"Mot de passe";
+			// 
+			// passwordBox
+			// 
+			this->passwordBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->passwordBox->Location = System::Drawing::Point(243, 265);
+			this->passwordBox->Name = L"passwordBox";
+			this->passwordBox->PasswordChar = '*';
+			this->passwordBox->Size = System::Drawing::Size(278, 22);
+			this->passwordBox->TabIndex = 10;
+			this->passwordBox->UseSystemPasswordChar = true;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(131, 227);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(106, 16);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"Nom d\'utilisateur";
+			// 
+			// usernameBox
+			// 
+			this->usernameBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->usernameBox->Location = System::Drawing::Point(243, 224);
+			this->usernameBox->Name = L"usernameBox";
+			this->usernameBox->Size = System::Drawing::Size(278, 22);
+			this->usernameBox->TabIndex = 8;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(103, 68);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(485, 90);
+			this->label4->TabIndex = 7;
+			this->label4->Text = resources->GetString(L"label4.Text");
+			this->label4->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
 			// button7
 			// 
 			this->button7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button7->Location = System::Drawing::Point(339, 327);
+			this->button7->Location = System::Drawing::Point(345, 456);
 			this->button7->Name = L"button7";
 			this->button7->Size = System::Drawing::Size(195, 49);
 			this->button7->TabIndex = 6;
@@ -201,7 +307,7 @@ namespace ProjetPOO {
 			// 
 			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button6->Location = System::Drawing::Point(138, 327);
+			this->button6->Location = System::Drawing::Point(144, 456);
 			this->button6->Name = L"button6";
 			this->button6->Size = System::Drawing::Size(195, 49);
 			this->button6->TabIndex = 5;
@@ -213,7 +319,7 @@ namespace ProjetPOO {
 			// 
 			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button5->Location = System::Drawing::Point(438, 272);
+			this->button5->Location = System::Drawing::Point(444, 401);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(213, 49);
 			this->button5->TabIndex = 4;
@@ -225,7 +331,7 @@ namespace ProjetPOO {
 			// 
 			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button4->Location = System::Drawing::Point(237, 272);
+			this->button4->Location = System::Drawing::Point(243, 401);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(195, 49);
 			this->button4->TabIndex = 3;
@@ -237,7 +343,7 @@ namespace ProjetPOO {
 			// 
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(16, 272);
+			this->button3->Location = System::Drawing::Point(22, 401);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(215, 49);
 			this->button3->TabIndex = 2;
@@ -250,18 +356,18 @@ namespace ProjetPOO {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(6, 99);
+			this->label3->Location = System::Drawing::Point(295, 354);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(114, 24);
+			this->label3->Size = System::Drawing::Size(104, 24);
 			this->label3->TabIndex = 1;
-			this->label3->Text = L"Raccourcis :";
+			this->label3->Text = L"Raccourcis";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(76, 3);
+			this->label2->Location = System::Drawing::Point(84, 18);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(522, 37);
 			this->label2->TabIndex = 0;
@@ -324,7 +430,9 @@ namespace ProjetPOO {
 			this->ClientSize = System::Drawing::Size(1522, 572);
 			this->Controls->Add(this->tabController);
 			this->Controls->Add(this->dataGridView);
-			this->Name = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->MaximizeBox = false;
+			//this->Name = L"Projet";
 			this->Text = L"Business Manager - Powered by EVAL";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView))->EndInit();
 			this->tabController->ResumeLayout(false);
@@ -358,6 +466,9 @@ private: System::Void clickOnClientsTab(System::Object^ sender, System::EventArg
 }
 private: System::Void clickOnStatistiquesTab(System::Object^ sender, System::EventArgs^ e) {
 	this->tabController->SelectedIndex = STATISTIQUES_TAB;
+}
+private: System::Void clickOnConnexionBDD(System::Object^ sender, System::EventArgs^ e) {
+	this->messageConnexionBDD->Text = "Connexion en cours...";
 }
 };
 }
